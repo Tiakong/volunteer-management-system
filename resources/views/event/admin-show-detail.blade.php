e537e56e4fa36ccbe517e875cf4868f2fe6ab2e9
Thean Chun <lolx2012@gmail.com>
2019-08-17 18:27:11 +0800

    new event interface

--- resources/views/event/admin-show-detail.blade.php
+++ resources/views/event/admin-show-detail.blade.php
@@ -21,24 +21,18 @@
                         <p class="event-info-font">{{$event->venue}}</p>
                     </div>
                     <div  class="event-detail-venue">
-                    @if(strtotime(date('Y/m/d')) < strtotime($event->date))
-                    <button class="event-detail-button btn btn-warning">{!! link_to_route('event.edit',
-                                $title = 'Edit Event',
-                                $parameters = [
-                                    'id' => $event->eid])!!}</button>
-                    <button class="event-detail-button btn btn-danger" onclick="return confirm('Do you want to delete this event?');">{!! link_to_route('event.destroy',
-                                $title = 'Remove Event',
-                                $parameters = [
-                                    'id' => $event->eid])!!}</button>
-                    @endif
-                    <button class="event-detail-button btn btn-primary" >{!! link_to_route('volunteer_event.index',
-                                $title = 'Volunteer List',
-                                $parameters = [
-                                    'id' => $event->eid])!!}</button>
+                    
+					<a href="{{route('event.edit', $event->eid)}}">
+						<button class="event-detail-button btn  btn-success"><i class='fa fa-edit'></i><b> Edit Event</b></button>
+					</a>
+					<a href="{{route('event.destroy', $event->eid)}}">
+						<button class="event-detail-button btn btn-danger" onclick="return confirm('Do you want to delete this event?');"><b>Remove Event</b></button>
+					</a>
+					<a href="{{route('volunteer_event.index',$event->eid)}}">
+						<button class="event-detail-button btn btn-primary" >Volunteer List</button></a>
                     </div>
                 </div>
             </div>
-
             <div class="event-detail-right-section">
                 <div class="event-detail-topright-section">
                     <img src="/storage/cover_image/{{$event->cover_image}}" width="100%" height="100%">
@@ -47,16 +41,12 @@
                     <p>{{$event->description}}
                 </div>
             </div>
-    </div>
-
-    <button class="event-detail-button btn btn-primary">{!! link_to_route('event.index',
-                                $title = 'Back')!!}</button>
-
-    
+			<a href="{{route('event.index')}}"><button class="event-detail-button btn btn-primary"><b>Back</b></button></a>
+	</div>
 
     <script>
-    var msg = '{{Session::get('alert')}}';
-    var exist = '{{Session::has('alert')}}';
+    var msg = '{{Session::get("alert")}}';
+    var exist = '{{Session::has("alert")}}';
     if(exist){
       alert(msg);
     }
